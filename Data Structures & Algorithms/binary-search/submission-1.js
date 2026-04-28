0909ab42eaa12e677754ac00d25c5809c6efad51
Ramik Mukherjee <55507277+ramik10@users.noreply.github.com>
2026-04-28 11:16:43 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left  = 0
+        let right = nums.length -1 
+        while(left<=right){
+            const mid = Math.floor(left + (right-left)/2)
+            if(target===nums[mid]){
+                return mid
+            }
+            if(target<nums[mid]){
+                right=mid - 1
+            }
+            if(target>nums[mid]){
+                left=mid + 1
+            }
+        }
+        return -1
+    }
+}
